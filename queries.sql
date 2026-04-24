59451d40ffd3f8317c4d14b98ed4963ab0205d16
nayoolwin2019-commits <nayoolwin2019@icloud.com>
2026-04-24 17:14:48 +0700

    queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,14 @@
+-- Create a table for Employees
+CREATE TABLE Employees (
+    ID int,
+    Name varchar(255),
+    Position varchar(255)
+);
+
+-- Insert your info
+INSERT INTO Employees (ID, Name, Position)
+VALUES (1, 'Iloveyou', 'Chit Su Thansin');
+
+-- Show the data
+SELECT * FROM Employees;
 
